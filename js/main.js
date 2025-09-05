c0d53859bfc3db1d8b2fb62220cfd6adf00bb777
seyeon326 <lseyeon0723@gmail.com>
2025-09-06 00:09:45 +0900

    promotion auto swipe

--- js/main.js
+++ js/main.js
@@ -49,7 +49,16 @@ fadeEls.forEach(function (fadeEl, index) {
 
 // new Swiper(선택자, 옵션)
 new Swiper('.notice-line .swiper', {
-  direction: 'vertical',
+  direction: 'vertical',  // 기본값은 horizontal
   autoplay: true,
   loop: true
+});
+new Swiper('.promotion .swiper', {
+  slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+  loop: true,
+  autoplay: {
+    delay: 5000
+  }
 });
