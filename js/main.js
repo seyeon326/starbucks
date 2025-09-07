51f43e597edd29b1838139b17d7ea54c5cc215ce
seyeon326 <lseyeon0723@gmail.com>
2025-09-07 23:14:06 +0900

    promotion slide toggle

--- js/main.js
+++ js/main.js
@@ -70,3 +70,16 @@ new Swiper('.promotion .swiper', {
     nextEl: '.promotion .swiper-next'
   }
 });
+
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function () {
+  isHidePromotion = !isHidePromotion;
+  if (isHidePromotion) {
+    promotionEl.classList.add('hide');
+  } else {
+    promotionEl.classList.remove('hide');
+  }
+});
