406849a3ceb9db2a7a3bdaebbf99d98e63484fbf
seyeon326 <lseyeon0723@gmail.com>
2025-09-08 21:07:39 +0900

    repeat animation

--- js/main.js
+++ js/main.js
@@ -83,3 +83,27 @@ promotionToggleBtn.addEventListener('click', function () {
     promotionEl.classList.remove('hide');
   }
 });
+
+
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2));
+}
+
+function floatingObject(selector, delay, size) {
+  gsap.to(
+    selector, // 선택자
+    random(1.5, 2.5),   // 애니메이션 동작 시간
+    {   // 옵션
+      y: size,
+      repeat: -1,  // 무한반복
+      yoyo: true,  // 내려갔다 올라갔다
+      ease: "power1.inOut",
+      delay: random(0, delay)
+    }
+  );
+}
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', .5, 15);
+floatingObject('.floating3', 1.5, 20);
